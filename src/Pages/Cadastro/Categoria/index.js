b74f815d6481031ba1d87067443409ca6647185c
Wellington Andrade <weg.andrade@gmail.com>
2020-08-03 17:17:32 -0300

    Utilizando  JSON SERVER para persistencia de dados

--- src/Pages/Cadastro/Categoria/index.js
+++ src/Pages/Cadastro/Categoria/index.js
@@ -90,7 +90,14 @@ function CadastroCategoria() {
           Cadastrar
         </Button>
       </form>
-
+      {
+        categorias.length === 0
+         && (
+         <div>
+           Loading ...
+         </div>
+         )
+      }
       <ul>
         {categorias.map((categoria) => (
           <li key={`${categoria.nome}`}>
